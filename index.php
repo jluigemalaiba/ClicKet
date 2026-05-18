da6aed6c29f7d7e728ba3dc902427a6c5b618086
Trisha Talas <talastrishamae@gmail.com>
2026-05-18 17:18:55 +0800

    adding a file

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$pageTitle = 'Home';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle; ?></title>
+</head>
+<body>
+
+</body>
+</html>
